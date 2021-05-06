04edb0c8560e0a663892851bb6b4e3c35eb5ee15
Paweł Waśkiewicz <pawel.waskiewicz@gmail.com>
2021-05-06 16:36:30 +0200

    Add JS and ...

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
 const hamburgerButton = document.querySelector('.hamburger-btn');
 
-const menu = document.querySelector('.main-menu-container');
+const menu = document.querySelector('.phone-sidebar');
 
 hamburgerButton.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
   console.log('clicked');
   menu.classList.toggle('showMenu');
+}
+
+const hamburgerButton2 = document.querySelector('.hamburger-btn-2');
+
+const Menu = document.querySelector('.phone-sidebar');
+
+hamburgerButton2.addEventListener('click', toggleMenu2);
+
+function toggleMenu2() {
+  console.log('clicked');
+  Menu.classList.toggle('showMenu');
 }
